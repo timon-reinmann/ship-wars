f5cc9ba47a51b6fb15528f1d3146c52b599e1c3d
Timon Reinmann <timon.reinmann@yoo.digital>
2023-08-09 15:31:03 +0200

    feat: I'have done the first step TRAINEE-165

--- YOO.Trainees.Christmastree.Timon/YOO.Trainees.Christmastree.Timon/Program.cs
+++ YOO.Trainees.Christmastree.Timon/YOO.Trainees.Christmastree.Timon/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World");
-Console.ReadKey();
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        var height = 16;
+        var star = "*";
+        var starDecoration = "o*";
+        var treeIntent = height - 2;
+        var stemIntent = height - 3;
+        var space = " ";
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = treeIntent; 0 <= j; j--)
+            {
+                Console.Write(space);
+            }
+            treeIntent--;
+            Console.WriteLine(star);
+            star += starDecoration;
+        }
+        for (int i = stemIntent; 0 <= i; i--)
+        {
+            Console.Write(space);
+        }
+        Console.WriteLine("| |");
+    }
+}
