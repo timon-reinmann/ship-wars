a655f3e41bd9dd43efbfc5bd70d222e502279531
Timon Reinmann <timon.reinmann@yoo.digital>
2023-08-15 08:41:53 +0200

    idk

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("enter the first letter of future:  ");
+Console.ReadKey();
+Console.WriteLine("");
+Console.WriteLine("No, that's wrong, try again: ");
+Console.ReadKey();
+Console.WriteLine("");
+Console.WriteLine("No, that's wrong, its L for: Liya I love you<3");
+Console.ReadKey();
--- YOO.Trainees.Christmastree.Timon/YOO.Trainees.Christmastree.Timon/Program.cs
+++ YOO.Trainees.Christmastree.Timon/YOO.Trainees.Christmastree.Timon/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.Design;
+using System.Drawing;
 using System.Reflection.Metadata.Ecma335;
+using static System.Net.Mime.MediaTypeNames;
 
 public class Program
 {
@@ -7,6 +9,9 @@ public class Program
     {
         int height;
         int color;
+        bool visible = true;
+        var rnd = new Random();
+        var colorDecoration = Console.ForegroundColor = ConsoleColor.White;
 
         Console.WriteLine("enter the desired height of your christmas tree:");
         while (!int.TryParse(Console.ReadLine(), out height))
@@ -23,130 +28,147 @@ public class Program
             Console.WriteLine("that isn't a number, enter a new number:");
         }
 
+        Console.WriteLine("should your tree blink (yes/no):");
+        string blink = Console.ReadLine();
 
-        var star = "*";
-        var star2 = "**";
-        var starDecoration = "o*";
-        var treeIntent = height - 2;
-        var stemIntent = height - 3;
-        var space = " ";
-        var colorDecoration = Console.ForegroundColor = ConsoleColor.White;
-        var rnd = new Random();
+            if (1 == color)
+            {
+                colorDecoration = ConsoleColor.Blue;
+            }
+            if (2 == color)
+            {
+                colorDecoration = ConsoleColor.Green;
+            }
+            if (3 == color)
+            {
+                colorDecoration = ConsoleColor.Red;
+            }
+            if (4 == color)
+            {
+                colorDecoration = ConsoleColor.Yellow;
+            }
+            if (5 == color)
+            {
+                colorDecoration = ConsoleColor.Magenta;
+            }
+            if (6 == color)
+            {
+                colorDecoration = ConsoleColor.White;
+            }
+            if (7 == color)
+            {
+                do
+                {
+                    var star = "*";
+                    var star2 = "**";
+                    var starDecoration = "o*";
+                    var treeIntent = height - 2;
+                    var stemIntent = height - 3;
+                    var space = " ";
+                    if (answer == "yes") { }
+                    else
+                    {
+                        starDecoration = star2;
+                    }
 
+                    for (int i = 0; i < height; i++)
+                    {
+                        for (int j = treeIntent; 0 <= j; j--)
+                        {
+                            Console.Write(space);
+                        }
 
-        if (1 == color)
-        {
-            colorDecoration = ConsoleColor.Blue;
-        }
-        if (2 == color)
-        {
-            colorDecoration = ConsoleColor.Green;
-        }
-        if (3 == color)
-        {
-            colorDecoration = ConsoleColor.Red;
-        }
-        if (4 == color)
-        {
-            colorDecoration = ConsoleColor.Yellow;
-        }
-        if (5 == color)
-        {
-            colorDecoration = ConsoleColor.Magenta;
-        }
-        if (6 == color)
-        {
-            colorDecoration = ConsoleColor.White;
-        }
-        if (7 == color)
-        {
-            if (answer == "yes") { }
+                        treeIntent--;
+
+                        foreach (char c in star)
+                        {
+                            if (c == '*')
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(c);
+                                Console.ResetColor();
+                            }
+                            else if (c == 'o')
+                            {
+                                Console.ForegroundColor = (ConsoleColor)rnd.Next(9, 16);
+                                Console.Write(c);
+                                Console.ResetColor();
+
+                            }
+                        }
+                        Console.WriteLine();
+                        star += starDecoration;
+                    }
+
+                    for (int i = stemIntent; 0 <= i; i--)
+                    {
+                        Console.Write(space);
+                    }
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("| |");
+                    Console.ResetColor();
+                } while (true);
+
+            }
             else
             {
-                starDecoration = star2;
-            }
-
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = treeIntent; 0 <= j; j--)
+                do {
+                var star = "*";
+                var star2 = "**";
+                var starDecoration = "o*";
+                var treeIntent = height - 2;
+                var stemIntent = height - 3;
+                var space = " ";
+                if (answer == "yes") { }
+                /*else
                 {
-                    Console.Write(space);
-                }
-
-                treeIntent--;
-
-                foreach (char c in star)
+                    starDecoration = star2;
+                }*/
+                for (int i = 0; i < height; i++)
                 {
-                    if (c == '*')
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(c);
-                        Console.ResetColor();
+                    for (int j = treeIntent; 0 <= j; j--)
+                        {
+                            Console.Write(space);
+                        }
+
+                        treeIntent--;
+
+                        foreach (char c in star)
+                        {
+                            if (c == '*')
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(c);
+                            }
+                            else if (c == 'o')
+                            {
+                                Console.ForegroundColor = visible ? ConsoleColor.White : (ConsoleColor)color;
+                                visible = !visible;
+                                Console.Write(c);
+                            }
+                        }
+                        Console.WriteLine();
+                        star += starDecoration;
                     }
-                    else if (c == 'o')
+
+                    for (int i = stemIntent; 0 <= i; i--)
                     {
-                        Console.ForegroundColor = (ConsoleColor)rnd.Next(9, 16);
-                        Console.Write(c);
-                        Console.ResetColor();
-
+                        Console.Write(space);
                     }
-                }
-                Console.WriteLine();
-                star += starDecoration;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("| |");
+                    Console.ResetColor();
+                    Thread.Sleep(500);
+                    Console.Clear();
+                } while (true);
+
             }
-
-            for (int i = stemIntent; 0 <= i; i--)
-            {
-                Console.Write(space);
-            }
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("| |");
-            Console.ResetColor();
-        }
-        else
-        {
-            if (answer == "yes") { }
-            else
-            {
-                starDecoration = star2;
-            }
-
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = treeIntent; 0 <= j; j--)
-                {
-                    Console.Write(space);
-                }
-
-                treeIntent--;
-
-                foreach (char c in star)
-                {
-                    if (c == '*')
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(c);
-                        Console.ResetColor();
-                    }
-                    else if (c == 'o')
-                    {
-                        Console.ForegroundColor = colorDecoration;
-                        Console.Write(c);
-                        Console.ResetColor();
-
-                    }
-                }
-                Console.WriteLine();
-                star += starDecoration;
-            }
-
-            for (int i = stemIntent; 0 <= i; i--)
-            {
-                Console.Write(space);
-            }
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("| |");
-            Console.ResetColor();
-        }
     }
+
 }
+
+
+
+
+
+
