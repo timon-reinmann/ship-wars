093ff78ebb818af75e342864fe2d7568f1f1c4cf
alex uscata <alex.uscata@yoo.digital>
2023-08-30 10:18:03 +0200

    feat: inviteController #Trainee-168

---
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api/Controllers/inviteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Yoo.Trainees.ShipWars.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class inviteController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+    }
+}
--- Yoo.Trainees.WarShip/Frontend/js/invite.js
+++ Yoo.Trainees.WarShip/Frontend/js/invite.js
@@ -1,3 +1,23 @@
+'use strict';
+
+const API_URL = 'https://localhost:7118//api/invite';
+fetch(API_URL, {
+    "credentials": "omit",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
+        "Accept": "*/*",
+        "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
+        "Content-Type": "application/json",
+        "Sec-Fetch-Dest": "empty",
+    },
+    "body": 'ERSETZTEN',
+    "method": "POST",
+})
+.then(response => response.json())
+.catch(error => {
+    console.error("Es gab einen Fehler bei der Anfrage:", error);
+});
+
 let copyText = document.querySelector(".copy-text");
 copyText.querySelector("button").addEventListener("click",function(){
     let input = copyText.querySelector("input.text");
