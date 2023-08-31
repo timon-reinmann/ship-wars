b8d51824790ad10da0808a58ff1f2b546c533b38
alex <alex.uscata@yoo.digital>
2023-08-31 15:25:49 +0200

    feat: game board created #TRAINEE-168

--- Yoo.Trainees.WarShip/Frontend/js/game.js
+++ Yoo.Trainees.WarShip/Frontend/js/game.js
@@ -1,5 +1,5 @@
 let n = 0;
-let game = document.getElementById("game__board")
+let game = document.getElementById("game__board");
 for (let x = 0; x < 10; x++) {
   for (let y = 0; y < 10; y++) {
     let div = document.createElement("div");
@@ -8,6 +8,19 @@ for (let x = 0; x < 10; x++) {
     div.setAttribute("data-x", x);
     div.setAttribute("data-y", y);
     game.appendChild(div);
+    n+=1;
+  }
+}
+n = 0;
+let gameOpponent = document.getElementById("opponent__board");
+for (let x = 0; x < 10; x++) {
+  for (let y = 0; y < 10; y++) {
+    let div = document.createElement("div");
+    div.classList.add("field");
+    div.classList.add(`b${n}`);
+    div.setAttribute("data-x", x);
+    div.setAttribute("data-y", y);
+    gameOpponent.appendChild(div);
     n+=1;
   }
 }
