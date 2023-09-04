d3b392bfbd28ffb7dc62942e36d0771261d2c966
alex uscata <alex.uscata@yoo.digital>
2023-09-04 13:57:48 +0200

    dsad

--- Yoo.Trainees.WarShip/Frontend/js/game.js
+++ Yoo.Trainees.WarShip/Frontend/js/game.js
@@ -1,12 +1,11 @@
 let n = 0;
 let game = document.getElementById("game__board");
-for (let x = 0; x < 10; x++) {
-  for (let y = 0; y < 10; y++) {
+for (let y = 0; y < 10; y++) {
+  for (let x = 0; x < 10; x++) {
     let div = document.createElement("div");
     div.classList.add("field");
     div.classList.add("ownField");
     div.classList.add(`b${n}`);
-    div.setAttribute("draggable", "true")
     div.setAttribute("data-x", x);
     div.setAttribute("data-y", y);
     game.appendChild(div);
@@ -26,10 +25,14 @@ for (let x = 0; x < 10; x++) {
     n+=1;
   }
 }
+let dragStartX = 0;
+let shipOffsetX = 0;
 const draggables = document.querySelectorAll('.ship');
 const containers = document.querySelectorAll('.ownField');
 draggables.forEach(draggable => {
-  draggable.addEventListener('dragstart', () => {
+  draggable.addEventListener('dragstart', (e) => {
+    dragStartX = e.clientX;
+    shipOffsetX = dragStartX - draggable.getBoundingClientRect().left;
     draggable.classList.add('dragging');
   });
   draggable.addEventListener('dragend', () => {
@@ -39,8 +42,17 @@ draggables.forEach(draggable => {
 containers.forEach(container => {
   container.addEventListener('dragover', e => {
     e.preventDefault();
+    
     const draggable = document.querySelector('.dragging');
-    container.appendChild(draggable);
+    const shipSize = parseInt(draggable.getAttribute("data-size"));
+
+    const currentX = parseInt(container.getAttribute("data-x"));
+
+    if (currentX + shipSize <= 10) {
+      container.appendChild(draggable);
+    } else {
+
+    }
   });
 });
 'use strict';
