dc8aee31224cb3dfbb76243c4179f9b2f258bef5
Timon Reinmann <timon.reinmann@yoo.digital>
2023-09-06 17:13:59 +0200

    feat: create email function

--- Yoo.Trainees.WarShip/Frontend/js/invite.js
+++ Yoo.Trainees.WarShip/Frontend/js/invite.js
@@ -11,8 +11,45 @@ copyText.querySelector("button").addEventListener("click",function(){
 });
 
 
+
+
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+
+const send_email = document.getElementById("email");
+email.addEventListener("click",function(){
+
+let email = document.getElementById("email-input").value;
+
+});
+
+
+
+
 const submit_button = document.getElementById("lobbyinput");
-submit_button.addEventListener("click", async function() {
+submit_button.addEventListener("click", function() {
 
     
     let createGame = document.querySelector(".submit__lobbyname");
@@ -22,10 +59,10 @@ submit_button.addEventListener("click", async function() {
     },2500);
 
     let lobbyName = JSON.stringify(document.getElementById("lobbyname").value);
-    let link = null;
+
     'use strict';
     const API_URL = 'https://localhost:7118/api/Game';
-    await fetch(API_URL, {
+    fetch(API_URL, {
         "credentials": "omit",
         "headers": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
@@ -40,41 +77,11 @@ submit_button.addEventListener("click", async function() {
     .then(response => response.json())
     .then(data => {
         console.log("Daten:", data);
-        link = "http://127.0.0.1:5500/Frontend/html/game-pvp.html?playerid=" +  data.id; 
-        document.getElementById('linkoutput').value = link;
-    })
-    .catch(error => {
-        console.error("Es gab einen Fehler bei der Anfrage:", error);
-    });
-
-    // ----------------------EMAIL------------------------------------
-        let data = {
-        gameName: lobbyName,
-        email: email,
-        link: link
-    };
-    data = JSON.stringify(data);
-    'use strict';
-
-    const API_URL_Email = 'https://localhost:7118/api/Game/Email';
-    fetch(API_URL_Email, {
-        "credentials": "omit",
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
-            "Accept": "*/*",
-            "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
-            "Content-Type": "application/json",
-            "Sec-Fetch-Dest": "empty",
-        },
-        "body": data,
-        "method": "POST",
-    })  
-    .then(response => response.json())
-    .then(data => {
-        console.log("Daten");
+        document.getElementById('linkoutput').value = "http://127.0.0.1:5500/Frontend/html/game-pvp.html?playerid=" +  data.id;
     })
     .catch(error => {
         console.error("Es gab einen Fehler bei der Anfrage:", error);
     });
 
 });
+
