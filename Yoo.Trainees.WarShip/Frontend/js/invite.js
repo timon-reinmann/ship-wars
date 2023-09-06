cdbcd860946cb24dbff2fd27d43db0290a6eda90
alex uscata <alex.uscata@yoo.digital>
2023-09-06 17:24:44 +0200

    feat: code saved merge #Trainee-168

--- Yoo.Trainees.WarShip/Frontend/js/invite.js
+++ Yoo.Trainees.WarShip/Frontend/js/invite.js
@@ -10,11 +10,6 @@ copyText.querySelector("button").addEventListener("click",function(){
     },2500);
 });
 
-
-
-
-
-
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
@@ -42,14 +37,11 @@ const send_email = document.getElementById("email");
 email.addEventListener("click",function(){
 
 let email = document.getElementById("email-input").value;
-
+console.log(email);
 });
 
-
-
-
 const submit_button = document.getElementById("lobbyinput");
-submit_button.addEventListener("click", function() {
+submit_button.addEventListener("click", async function() {
 
     
     let createGame = document.querySelector(".submit__lobbyname");
@@ -59,10 +51,10 @@ submit_button.addEventListener("click", function() {
     },2500);
 
     let lobbyName = JSON.stringify(document.getElementById("lobbyname").value);
-
+    let link = null;
     'use strict';
     const API_URL = 'https://localhost:7118/api/Game';
-    fetch(API_URL, {
+    await fetch(API_URL, {
         "credentials": "omit",
         "headers": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
@@ -77,11 +69,42 @@ submit_button.addEventListener("click", function() {
     .then(response => response.json())
     .then(data => {
         console.log("Daten:", data);
-        document.getElementById('linkoutput').value = "http://127.0.0.1:5500/Frontend/html/game-pvp.html?playerid=" +  data.id;
+        link = "http://127.0.0.1:5500/Frontend/html/game-pvp.html?playerid=" +  data.id; 
+        document.getElementById('linkoutput').value = link;
     })
     .catch(error => {
         console.error("Es gab einen Fehler bei der Anfrage:", error);
     });
 
-});
+    // ----------------------EMAIL------------------------------------
+        let data = {
+        gameName: lobbyName,
+        email: email,
+        link: link
+    };
+    data = JSON.stringify(data);
+    'use strict';
 
+    const API_URL_Email = 'https://localhost:7118/api/Game/Email';
+    fetch(API_URL_Email, {
+        "credentials": "omit",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
+            "Accept": "*/*",
+            "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
+            "Content-Type": "application/json",
+            "Sec-Fetch-Dest": "empty",
+        },
+        "body": data,
+        "method": "POST",
+    })  
+    .then(response => response.json())
+    .then(data => {
+        console.log("Daten");
+    })
+    .catch(error => {
+        console.error("Es gab einen Fehler bei der Anfrage:", error);
+    });
+    
+
+});
