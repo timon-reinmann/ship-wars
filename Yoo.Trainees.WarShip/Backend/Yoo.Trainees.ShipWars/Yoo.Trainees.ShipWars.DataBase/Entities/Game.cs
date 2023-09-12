697f59ead56e736a5f0a874e8a4e21a3bf8070c0
alex uscata <alex.uscata@yoo.digital>
2023-09-12 10:19:11 +0200

    fix: change id name #Trainee-168

--- Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.DataBase/Entities/Game.cs
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.DataBase/Entities/Game.cs
@@ -5,16 +5,16 @@ namespace Yoo.Trainees.ShipWars.DataBase.Entities
 {
     public class Game
     { 
-        public Guid Id { get; set; }
+        public Guid Player1Id { get; set; }
         public Guid Player2Id { get; set; }
         public string Name { get; set; }
         //smalldatetime => 1900-01-01 00:00:00 bis 2079-06-06 23:59:59
         [Column(TypeName = "smalldatetime")]
         public DateTime Date { get; set; }
 
-        public Game(Guid id, Guid player2Id, string name)
+        public Game(Guid player1Id, Guid player2Id, string name)
         {
-            Id = id;
+            Player1Id = player1Id;
             Player2Id = player2Id;
             Name = name;
             Date = DateTime.Now;
