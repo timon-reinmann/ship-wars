aeb3673c22d96e74b5a0f38d75d81b40c28275d2
alex uscata <alex.uscata@yoo.digital>
2023-09-12 11:39:59 +0200

    feat: umsetztung erd #Trainee-168

--- Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.DataBase/Entities/Game.cs
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.DataBase/Entities/Game.cs
@@ -4,19 +4,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Yoo.Trainees.ShipWars.DataBase.Entities
 {
     public class Game
-    { 
-        public Guid Player1Id { get; set; }
-        public Guid Player2Id { get; set; }
+    {
+        public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Game_Status { get; set; }
         //smalldatetime => 1900-01-01 00:00:00 bis 2079-06-06 23:59:59
         [Column(TypeName = "smalldatetime")]
         public DateTime Date { get; set; }
 
-        public Game(Guid player1Id, Guid player2Id, string name)
+        public Game(Guid id, string name, string game_Status)
         {
-            Player1Id = player1Id;
-            Player2Id = player2Id;
+            Id = id;
             Name = name;
+            Game_Status = game_Status;
             Date = DateTime.Now;
         }
     }
---
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.DataBase/Entities/Player.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Yoo.Trainees.ShipWars.DataBase.Entities
+{
+    public class player
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public player(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
