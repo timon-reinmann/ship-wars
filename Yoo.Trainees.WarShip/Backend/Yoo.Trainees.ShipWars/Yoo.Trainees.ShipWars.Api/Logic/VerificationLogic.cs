39395f4ece01510340ae526a5e88488a050544fc
Timon Reinmann <timon.reinmann@yoo.digital>
2023-10-09 11:52:11 +0200

    feat: delete commend code

--- Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api/Logic/VerificationLogic.cs
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api/Logic/VerificationLogic.cs
@@ -16,50 +16,6 @@ namespace Yoo.Trainees.ShipWars.Api.Logic
             this.ships = ships;
         }
 
-        //public bool VerifyShipLocations(SaveShipDto[] shipDtos)
-        //{
-        //    if (shipDtos.Length != 10)
-        //    {
-        //        return false;
-        //    }
-
-        //    // mehr checks
-
-
-        //    int[,] playingFieldBoarder = new int[9, 9];
-        //    int[,] playingFieldShip = new int[9, 9];
-
-        //    for (int i = 0; i < shipDtos.Length; i++)
-        //    {
-        //        var shipType = ships.SingleOrDefault(x => x.Name == shipDtos[i].ShipType);
-        //        if (shipDtos[i].X != 0 && shipDtos[i].X + shipType.Length - 1 != 9 && shipDtos[i].Y != 0 && shipDtos[i].Y != 9)
-        //        {
-
-        //            for (int j = 0; j < shipType.Length - 1; j++)
-        //            {
-        //                for (int k = 0; k < 3 - 1; k++)
-        //                {
-        //                        playingFieldBoarder[j + shipDtos[i].X - 1, k + shipDtos[i].Y - 1] = 1;
-        //                }
-        //            }
-        //            for (int j = 0; j  <= shipType.Length; j++)
-        //            {
-        //                if (playingFieldBoarder[j + shipDtos[i].X, shipDtos[i].Y] == 1)
-        //                {
-        //                    return false;
-        //                }
-        //                else
-        //                {
-        //                    playingFieldShip[j + shipDtos[i].X, shipDtos[i].Y] = 1;
-        //                }
-        //            }
-        //        }
-        //    }
-
-
-        //    return true;
-        //}
-
         public bool VerifyShipLocations(SaveShipDto[] shipDtos)
         {
             if (shipDtos.Length != 10)
