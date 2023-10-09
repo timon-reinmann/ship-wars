c5c09aeff34f6216e3c0e1f3fa5c3e43f11149d3
Timon Reinmann <timon.reinmann@yoo.digital>
2023-10-09 13:57:30 +0200

    feat: Add other VerificationLogicTests

--- Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api.Test/VerificationLogicTest.cs
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api.Test/VerificationLogicTest.cs
@@ -36,16 +36,16 @@ namespace Yoo.Trainees.ShipWars.Api.Test
 
             var shipDtos = new SaveShipDto[]
             {
-                new SaveShipDto{Direction = "horizontal", X = 4, Y = 2, ShipType = "Warship"},
-                new SaveShipDto{Direction = "horizontal", X = 0, Y = 3, ShipType = "Cruiser"},
-                new SaveShipDto{Direction = "horizontal", X = 4, Y = 6, ShipType = "Cruiser"},
-                new SaveShipDto{Direction = "horizontal", X = 0, Y = 0, ShipType = "Destroyer"},
-                new SaveShipDto{Direction = "horizontal", X = 7, Y = 4, ShipType = "Destroyer"},
-                new SaveShipDto{Direction = "horizontal", X = 1, Y = 8, ShipType = "Destroyer"},
-                new SaveShipDto{Direction = "horizontal", X = 6, Y = 0, ShipType = "Submarine"},
-                new SaveShipDto{Direction = "horizontal", X = 1, Y = 5, ShipType = "Submarine"},
-                new SaveShipDto{Direction = "horizontal", X = 7, Y = 8, ShipType = "Submarine"},
-                new SaveShipDto{Direction = "horizontal", X = 4, Y = 9, ShipType = "Submarine"}
+                new SaveShipDto{Direction = "vertical", X = 1, Y = 0, ShipType = "Warship"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 1, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "vertical", X = 6, Y = 4, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "vertical", X = 3, Y = 3, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 8, Y = 3, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 3, Y = 6, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "vertical", X = 1, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "vertical", X = 4, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 8, Y = 7, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 6, Y = 9, ShipType = "Submarine"}
             };
 
             Assert.True(verificationLogic.VerifyShipLocations(shipDtos));
@@ -72,5 +72,94 @@ namespace Yoo.Trainees.ShipWars.Api.Test
 
             Assert.False(verificationLogic.VerifyShipLocations(shipDtos));
         }
+
+        [Test]
+        public void TestVerifyeOutOfBound_ShouldReturnFalse()
+        {
+            var verificationLogic = new VerificationLogic(_ships);
+
+            var shipDtos = new SaveShipDto[]
+            {
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 2, ShipType = "Warship"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 3, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 6, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 0, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 4, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 8, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 6, Y = 0, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 5, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 12, ShipType = "Submarine"}
+            };
+
+            Assert.False(verificationLogic.VerifyShipLocations(shipDtos));
+        }
+
+        [Test]
+        public void TestVerifyeUnkownShips_ShouldReturnFalse()
+        {
+            var verificationLogic = new VerificationLogic(_ships);
+
+            var shipDtos = new SaveShipDto[]
+            {
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 2, ShipType = "Warship"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 3, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 6, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 0, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 4, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 8, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 6, Y = 0, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 5, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 9, ShipType = "UnkownShip"}
+            };
+
+            Assert.False(verificationLogic.VerifyShipLocations(shipDtos));
+        }
+
+        [Test]
+        public void TestVerifyeToManyShipsFromSameType_ShouldReturnFalse()
+        {
+            var verificationLogic = new VerificationLogic(_ships);
+
+            var shipDtos = new SaveShipDto[]
+            {
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 2, ShipType = "Warship"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 3, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 6, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 0, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 4, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 6, Y = 0, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 5, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 9, ShipType = "Submarine"}
+            };
+
+            Assert.False(verificationLogic.VerifyShipLocations(shipDtos));
+        }
+        
+        [Test]
+        public void TestVerifyeUnkownDirection_ShouldReturnFalse()
+        {
+            var verificationLogic = new VerificationLogic(_ships);
+
+            var shipDtos = new SaveShipDto[]
+            {
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 2, ShipType = "Warship"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 3, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 4, Y = 6, ShipType = "Cruiser"},
+                new SaveShipDto{Direction = "horizontal", X = 0, Y = 0, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 4, ShipType = "Destroyer"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 6, Y = 0, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 1, Y = 5, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "horizontal", X = 7, Y = 8, ShipType = "Submarine"},
+                new SaveShipDto{Direction = "Unkown", X = 4, Y = 9, ShipType = "Submarine"}
+            };
+
+            Assert.False(verificationLogic.VerifyShipLocations(shipDtos));
+        }
+
     }
 }
--- Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api/Logic/VerificationLogic.cs
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api/Logic/VerificationLogic.cs
@@ -27,41 +27,71 @@ namespace Yoo.Trainees.ShipWars.Api.Logic
             {
                 var shipType = ships.SingleOrDefault(x => x.Name == shipDtos[i].ShipType);
                 var shipX = shipDtos[i].X;
-
                 var shipY = shipDtos[i].Y;
                 var shipLength = shipType.Length;
                 var _iY1 = shipY - 1;
                 var _iY2 = shipY + 1;
+                var _iX1 = shipX - 1;
+                var _iX2 = shipX + 1;
                 var shipDirection = shipDtos[i].Direction;
                 int _iXl;
-                foreach (var j in shipDtos)
+                int _iYl;
+                var allShips = new Dictionary<string, int>();
+
+                if (shipX > 9 || shipY > 9 || shipX < 0 || shipY < 0 || shipType == null)
                 {
+                    return false;
+                }
+
+                foreach (var _shipType in shipDtos)
+                {
+                    if (!allShips.ContainsKey(_shipType.ShipType))
+                    {
+                        allShips.Add(_shipType.ShipType, 1);
+                    }
+                    else
+                    {
+                        allShips[_shipType.ShipType]++;
+                    }
+                }
+                foreach (var c in allShips)
+                {
+                    if (c.Value == 1 && c.Key != "Warship" || c.Value == 2 && c.Key != "Cruiser" || c.Value == 3 && c.Key != "Destroyer" || c.Value == 4 && c.Key != "Submarine")
+                    {
+                        return false;
+                    }
+                }
+                foreach (var j in shipDtos)
                     for (int l = -1; l <= shipLength; l++)
                     {
                         _iXl = shipX + l;
+                        _iYl = shipY + l;
+
                         if (shipDtos[i] != j)
                         {
                             switch (shipDirection)
                             {
                                 case "horizontal":
-                                    if (j.X == _iXl && j.Y == shipY || j.X == _iXl && j.Y == shipY - 1 || j.X == _iXl && j.Y == _iY2)
+                                    if (j.X == _iXl && j.Y == shipY || j.X == _iXl && j.Y == _iY1 || j.X == _iXl && j.Y == _iY2)
                                     {
                                         return false;
                                     }
                                     break;
                                 case "vertical":
-                                    //todo
+                                    if (j.Y == _iYl && j.X == shipX || j.Y == _iYl && j.X == _iX1 || j.Y == _iYl && j.X == _iX2)
+                                    {
+                                        return false;
+                                    }
                                     break;
+                                default:
+                                    return false;
                             }
                         }
                     }
-                }
-
             }
             return true;
         }
-
-
     }
-
 }
+
+
