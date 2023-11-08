6552dbe33bca88cb752c29140a3ec13cf1b174a1
alex uscata <alex.uscata@yoo.digital>
2023-11-08 13:02:09 +0100

    refactor(GameHub:API): added CountShots & LoadShots #Trainee-190
    We changed the fetch method of CountShots and LoadShots from fetch to signalR
    refs: #Trainee-190

--- Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api/GameHub.cs
+++ Yoo.Trainees.WarShip/Backend/Yoo.Trainees.ShipWars/Yoo.Trainees.ShipWars.Api/GameHub.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Yoo.Trainees.ShipWars.Api.Logic;
 using Yoo.Trainees.ShipWars.DataBase;
 using Yoo.Trainees.ShipWars.DataBase.Entities;
 
@@ -7,9 +10,11 @@ namespace Yoo.Trainees.ShipWars.Api;
 public sealed class ChatHub : Hub
 {
     private readonly ApplicationDbContext _applicationDbContext;
-    public ChatHub(ApplicationDbContext applicationDbContext)
+    private readonly IGameLogic _gameLogic;
+    public ChatHub(ApplicationDbContext applicationDbContext, IGameLogic gameLogic)
     {
         _applicationDbContext = applicationDbContext;
+        _gameLogic = gameLogic;
     }
 
     public override async Task OnConnectedAsync()
@@ -45,10 +50,43 @@ public sealed class ChatHub : Hub
         };
 
         // send Message with ReceiveMessage Method
-        await Clients.Group(gamePlayer.GameId.ToString()).SendAsync("ReceiveMessage", player.Name, messageDB.Text, messageDB.Date);
+        await Clients.Group(GetGameId(user).ToString()).SendAsync("ReceiveMessage", player.Name, messageDB.Text, messageDB.Date);
 
         // add Message and save changes
         _applicationDbContext.Message.Add(messageDB);
         await _applicationDbContext.SaveChangesAsync();
+    }
+
+    // To visualize the own Fields which got hit.
+    [HttpGet("{gamePlayerId}/LoadShotsFromOpponent")]
+    public async Task LoadShotsFromOpponent(Guid gamePlayerId)
+    {
+        var gameId = await GetGameId(gamePlayerId);
+        var opponent = await GetOpponent(gamePlayerId, gameId);
+        var shots = _gameLogic.GetAllShotsOfOpponent(gamePlayerId);
+
+        await Clients.Groups(opponent.Id.ToString()).SendAsync("LoadShotsFromOpponent", shots);
+    }
+
+    // Count ALL shots for the counter and also give information for the nextplayer and game state (Ongoing, Lost, Won, Prep, Complete).
+    [HttpGet("{gamePlayerId}/CountShots")]
+    public async Task CountShots(Guid gamePlayerId)
+    {
+        ShotInfoDto countAndNextPlayer = _gameLogic.CountShotsInDB(gamePlayerId);
+        var gameStateDB = _gameLogic.GetGameState(gamePlayerId);
+        var gameId = await GetGameId(gamePlayerId);
+        var opponent = await GetOpponent(gamePlayerId, gameId);
+
+        await Clients.Group(opponent.ToString()).SendAsync("CountShots", new { shots = countAndNextPlayer.ShotCount, nextPlayer = countAndNextPlayer.IsNextPlayer, gameState = gameStateDB });
+    }
+
+    private async Task<GamePlayer?> GetOpponent(Guid gamePlayerId, Guid gameId)
+    {
+        return await (from gp in _applicationDbContext.GamePlayer where gp.GameId == gameId && gp.Id != gamePlayerId select gp).SingleOrDefaultAsync();
+    }
+
+    private async Task<Guid> GetGameId(Guid gamePlayerId)
+    {
+        return await (from gp in _applicationDbContext.GamePlayer where gp.Id.Equals(gamePlayerId) select gp.GameId).SingleOrDefaultAsync();
     }
 }
