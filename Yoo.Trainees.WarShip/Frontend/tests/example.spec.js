d21fdf5a165ce0a3835639e76365946e2a6f509c
alex uscata <alex.uscata@yoo.digital>
2024-01-03 16:05:20 +0100

    refactor(test): set ships test #Trainee-204
    #Trainee-204

--- Yoo.Trainees.WarShip/Frontend/tests/example.spec.js
+++ Yoo.Trainees.WarShip/Frontend/tests/example.spec.js
@@ -23,5 +23,45 @@ test("get started link", async ({ page }) => {
 
   await page.click("#button--copy");
 
-  await expect(page.getByText(/Join Game/)).toBeVisible();
+  await expect(page.getByText(/Game/)).toBeVisible();
+
+  await page.waitForTimeout(2000);
+
+  await page.getByText(/Game/).click();
+
+  await expect(page.getByText(/Commit/)).toBeVisible();
+});
+
+test("place ships", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/Frontend/html/invite.html");
+
+  await page.click("#lobbyinput");
+
+  await page.getByRole("button", { name: "Link for friend" }).click();
+
+  await page.click("#button--copy");
+
+  await page.waitForTimeout(2000);
+
+  await page.getByText(/Game/).click();
+
+  await page.locator("#warship1").dragTo(page.locator("#box0"));
+
+  await page.locator("#cruiser1").dragTo(page.locator("#box20"));
+
+  await page.locator("#cruiser1").dragTo(page.locator("#box40"));
+
+  await page.locator("#destroyer1").dragTo(page.locator("#box60"));
+
+  await page.locator("#destroyer2").dragTo(page.locator("#box80"));
+
+  await page.locator("#destroyer3").dragTo(page.locator("#box6"));
+
+  await page.locator("#submarine1").dragTo(page.locator("#box77"));
+
+  await page.locator("#submarine2").dragTo(page.locator("#box79"));
+
+  await page.locator("#submarine3").dragTo(page.locator("#box97"));
+
+  await page.locator("#submarine4").dragTo(page.locator("#box99"));
 });
