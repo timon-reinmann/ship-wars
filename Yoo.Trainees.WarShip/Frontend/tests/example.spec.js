9cfde9e79b658fe17a20ba161f9b8141d69f402d
alex uscata <alex.uscata@yoo.digital>
2024-01-03 12:51:54 +0100

    feat(FE): implememted Playwright NO TEST #Trainee-204
    refs: #Trainee-204

---
+++ Yoo.Trainees.WarShip/Frontend/tests/example.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("has title", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/Frontend/index.html");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/ShipWars/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/Frontend/index.html");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "VS Player" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByText(/Home/)).toBeVisible();
+});
